263fa5e944e5304f52fcc17e4ca7044550ebab6a
J. Tower <jtower@trailheadtechnology.com>
2023-04-27 15:48:29 -0400

    Reorg with long and short versions

---
+++ __dotnetconf-short-version/before/aspnetCore/Controller/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetCore.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1-core", "value2-core" };
+        }
+    }
+}
