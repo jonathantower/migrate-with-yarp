3f42fa7b372c86315868856b823cdc0c89cba743
J. Tower <jtower@trailheadtechnology.com>
2023-04-27 16:47:23 -0400

    before project updates

---
+++ code/before/eShopLegacy.Utilities/SomeClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace eShopLegacy.Utilities
+{
+    public class SomeClass
+    {
+        public static string GetHeader(HttpContext context)
+        {
+            return context.Request.Headers["User-Agent"];
+        }
+    }
+}
